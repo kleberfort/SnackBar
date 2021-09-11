e863bea42168f68e0bd8d2cc5da1d8444525f18c
kleberfort <kleberfort@gmail.com>
2021-09-11 15:52:12 -0300

    projeto modificado

--- app/src/main/java/com/example/aplicacaocomaversao42/MainActivity.java
+++ app/src/main/java/com/example/aplicacaocomaversao42/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.aplicacaocomaversao42;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int numero1, numero2, soma;
+
+   // TextView textoResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+       // textoResultado.findViewById(R.id.textResultado);
+
+       // soma = numero1 + numero2;
+        //textoResultado.setText("a soma é:");
+
     }
+
+
+
+
 }
